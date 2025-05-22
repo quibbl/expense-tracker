36f335ad3cd14ab95a11d433be6111f142520414
quibbl <3108a1990@gmail.com>
2025-05-22 12:52:34 +0200

    refactor: files structure (#5)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,15 @@
-import js from "@eslint/js";
 import globals from "globals";
-import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
+    plugins: { 
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh, }, 
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser }
+  },
 ]);
--- src/expenses/expenses.controller.ts
+++ src/expenses/expenses.controller.ts
@@ -1,16 +1,18 @@
-import express from 'express';
-import { addExpense, getExpenses } from '../services/expenses.service';
+import { Router } from 'express';
+import { addExpense, getExpenses } from './expenses.service';
 
-const router = express.Router();
+const router = Router();
 
 router.post('/expenses', async (req, res) => {
   const expense = req.body;
   const result = await addExpense(expense);
+  
   res.status(200).send({ success: true, id: result.id });
 });
 
 router.get('/expenses', (req, res) => {
   const expenses = getExpenses();
+
   res.send(expenses);
 });
 
