7a793b04cd46c197ce4f38ce63be4d154aaec06a
quibbl <3108a1990@gmail.com>
2025-05-27 12:06:58 +0200

    updated config files

---
+++ frontend/stylelint.config.mjs
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  plugins: [],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+        ],
+      },
+    ],
+  },
+  ignoreFiles: ['node_modules/**', 'dist/**'],
+};
