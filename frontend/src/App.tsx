bb1bc05386b65ec822b837a82f383d46c0928279
quibbl <3108a1990@gmail.com>
2026-02-17 13:06:42 +0100

    created button component (#12)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import Header from '@/components/Header/Header';
 
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <div>
       <Header />
+      <Button />
     </div>
   );
 }
